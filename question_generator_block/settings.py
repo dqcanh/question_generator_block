fffa0f33e7d7982490e154953bb15f33b824580b
Tam Minh Dang <delgemoon@gmail.com>
2017-06-05 12:33:15 +0700

    update database schema

--- question_generator_block/settings.py
+++ question_generator_block/settings.py
@@ -2,7 +2,7 @@ database = {
     'user': 'bn_edx',
   	'password': '458ce584b123',
   	'host': '127.0.0.1',
-  	'database': 'edxapp',
+  	'database': 'bn_edx',
   	'raise_on_warnings': True,
     'buffered': True,
 }
