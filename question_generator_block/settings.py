04b9e2a0e093e5549e9bc0b10d33a8b0b8147275
Tam Minh Dang <delgemoon@gmail.com>
2017-06-05 13:09:11 +0700

    update bn_edx to binami_edx

--- question_generator_block/settings.py
+++ question_generator_block/settings.py
@@ -2,7 +2,7 @@ database = {
     'user': 'bn_edx',
   	'password': '458ce584b123',
   	'host': '127.0.0.1',
-  	'database': 'bn_edx',
+  	'database': 'binami_edx',
   	'raise_on_warnings': True,
     'buffered': True,
 }
